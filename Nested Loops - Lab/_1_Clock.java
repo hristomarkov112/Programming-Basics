ecf357ca9111379df51c2b1756b532bf83d5e484
hristomarkov112 <146643476+hristomarkov112@users.noreply.github.com>
2024-03-24 19:13:25 +0200

    Create _1_Clock.java

---
+++ Nested Loops - Lab/_1_Clock.java
@@ -0,0 +1,10 @@
+public class _1_Clock {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 23; i++) {
+            for (int j = 0; j <= 59; j++) {
+                    System.out.println(i + ":" + j);
+            }
+        }
+    }
+}
