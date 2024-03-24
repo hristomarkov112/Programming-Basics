63ceba426b16f922672f75e6f67ea056aeded058
hristomarkov112 <146643476+hristomarkov112@users.noreply.github.com>
2024-03-24 18:35:04 +0200

    Update _1_ExcellentResult.java

--- Conditional Statements - Lab/_1_ExcellentResult.java
+++ Conditional Statements - Lab/_1_ExcellentResult.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
+
+public class _1_ExcellentResult {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int result = Integer.parseInt(sc.nextLine());
+
+        if (result >= 5) {
+            System.out.println("Excellent!");
+        }
+
+    }
+}
 
