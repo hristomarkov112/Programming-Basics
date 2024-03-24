88a085eff2c91cdbb09c2a1fa2876919faf70ee5
hristomarkov112 <146643476+hristomarkov112@users.noreply.github.com>
2024-03-24 18:55:04 +0200

    Create _1_NumbersEndingIn7.java

---
+++ For Loops - Exercise/_1_NumbersEndingIn7.java
@@ -0,0 +1,11 @@
+public class _1_NumbersEndingIn7 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 1000; i++) {
+            if(i % 10 == 7){
+                System.out.println(i);
+            }
+        }
+    }
+}
