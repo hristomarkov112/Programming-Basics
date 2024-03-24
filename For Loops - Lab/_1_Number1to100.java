cc798c689609b14e9a18c4a5821266ca0aa36391
hristomarkov112 <146643476+hristomarkov112@users.noreply.github.com>
2024-03-24 18:53:14 +0200

    Create _1_Number1to100.java

---
+++ For Loops - Lab/_1_Number1to100.java
@@ -0,0 +1,14 @@
+public class _1_Number1to100 {
+
+    public static void main(String[] args) {
+
+        // начало: 1
+        // край : 100
+        // инкремент: +1
+
+        for (int i = 1; i <= 100; i++){
+            System.out.println(i);
+        }
+
+    }
+}
